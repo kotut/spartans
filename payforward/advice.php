c1114f0d95335463850e9e22bf551eb2dd2e6458
kotut <lynwazim@yahoo.com>
2016-02-04 17:53:57 -0500

    DB - error corrections

--- payforward/advice.php
+++ payforward/advice.php
@@ -2,6 +2,12 @@
 
 include("../include/db.php"); 
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+echo "<h1>Pay it Forward Advices </h1>";
+
+
 $query = "SELECT * FROM payforward ORDER BY uid DESC" ;
 
 $result = mysqli_query($conn, $query);
