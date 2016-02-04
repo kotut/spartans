3554e08cc1ae906f200b23b6de679911aa648947
aamcfall <aaron.mcfall@yahoo.com>
2016-02-04 18:18:45 -0500

    trippy

--- meetup.php
+++ meetup.php
@@ -94,7 +94,7 @@
 		  <div class="form-group">
 			<label for="event_name" class="col-lg-2 control-label">Event Name</label>
 			<div class="col-lg-10">
-			  <input type="text" class="form-control" id="user-name" placeholder="Enter you name">
+			  <input type="text" class="form-control" id="user-name" placeholder="Event Name">
 			</div>
 		  </div><!-- End form group -->
 
@@ -102,21 +102,6 @@
 		  <div class="form-group">
 			<label for="event_time" class="col-lg-2 control-label">Date</label><br>
 			<label for="" class="col-lg-2 control-label"></label>
-			<select>
-             <option value="1">1</option>
-             <option value="2">2</option>
-			 <option value="3">3</option>
-			 <option value="4">4</option>
-			 <option value="5">5</option>
-             <option value="6">6</option>
-			 <option value="7">7</option>
-			 <option value="8">8</option>
-			 <option value="9">9</option>
-             <option value="10">10</option>
-			 <option value="11">11</option>
-			 <option value="12">12</option>
-			</select>
-
 				<select>
              <option value="1">Jan</option>
              <option value="2">Feb</option>
@@ -131,12 +116,46 @@
 			  <option value="11">Nov</option>
              <option value="12">Dec</option>
 			</select>
+			<select>
+             <option value="1">1</option>
+             <option value="2">2</option>
+			 <option value="3">3</option>
+			 <option value="4">4</option>
+			 <option value="5">5</option>
+             <option value="6">6</option>
+			 <option value="7">7</option>
+			 <option value="8">8</option>
+			 <option value="9">9</option>
+             <option value="10">10</option>
+			 <option value="11">11</option>
+			 <option value="12">12</option>
+			  <option value="13">13</option>
+			 <option value="14">14</option>
+			 <option value="15">15</option>
+             <option value="16">16</option>
+			 <option value="17">17</option>
+			 <option value="18">18</option>
+			 <option value="19">19</option>
+             <option value="20">20</option>
+             <option value="21">21</option>
+             <option value="22">22</option>
+			 <option value="23">23</option>
+			 <option value="24">24</option>
+			 <option value="25">25</option>
+             <option value="26">26</option>
+			 <option value="27">27</option>
+			 <option value="28">28</option>
+			 <option value="29">29</option>
+             <option value="30">30</option>
+            <option value="31">31</option>
+             <option value="32">32</option>
+			</select>
 		  </div><!-- End form group -->
 
 		   <div class="form-group">
 			<label for="event_name" class="col-lg-2 control-label">Location</label>
 			<div class="col-lg-10">
-			  <input type="text" class="form-control" id="user-name" placeholder="Enter you name">
+			  <input type="text" class="form-control" id="user-name" placeholder="Location">
 			</div>
 		  </div><!-- End form group -->
 		  
@@ -341,7 +360,7 @@
 		  <div class="form-group">
 			<label for="event_name" class="col-lg-2 control-label">Spaces</label>
 			<div class="col-lg-10">
-			  <input type="text" class="form-control" id="user-name" placeholder="Enter you name">
+			  <input type="text" class="form-control" id="user-name" placeholder="Available Spaces">
 			</div>
 		  </div><!-- End form group --> 
 		  
@@ -362,7 +381,7 @@
 			<label for="event_description" class="col-lg-2 control-label">Description</label>
 			<div class="col-lg-10">
 			  <textarea name="user-message" id="user-message" class="form-control" 
-			  cols="20" rows="10" placeholder="Enter your Message"></textarea>
+			  cols="20" rows="10" placeholder="Enter your Description..."></textarea>
 			</div>
 		  </div><!-- End form group -->
 
