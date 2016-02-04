a0d6cbd9b3a88b872be790a02eec8cf6065dcfd5
kotut <lynwazim@yahoo.com>
2016-02-04 16:37:46 -0500

    Pay Forward - UI
    Index UI part #1 done.

--- payforward/index.php
+++ payforward/index.php
@@ -1,54 +1,77 @@
 <html>
 <head> 
-
+	<title>Pay it Forward </title>
+	<link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="../css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+	<link href="../css/custom.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 
+<div class="container">
+
 <h1>Pay It Forward</h1>
+
 <form method="POST" action="preview-advice.php">
 
-	<p> <label for "title"> Title Post: </label> <input type="text" name="title" value="" required /> </p>
+	<p> <label for "title"> Title Post: </label> <input type="text" class="form-control"  name="title" value="" required /> </p>
 	
-	<p> <label for "advice"> Advice: </label> <br/><textarea name="advice" rows="10" cols="30"></textarea></p>
+	<p> <label for "advice"> Advice: </label> <br/><textarea name="advice" class="form-control" rows="10" cols="30"></textarea></p>
 	
 	<p> <label for "Name"> Your Name: </label> <input type="text" name="name" value="" required s/></p>
+		
+	<div class="row">
+		
+		<div class="col-md-3"> 
+			<p> <label for "role"> Role:  </label> 
+			<select name="role">
+				<option value="roles"> Select role </option>
+				<option value="current"> Current Student </option>
+				<option value="alumni"> Alumni </option>
+				<option value="citizen"> Concerned Citizen </option>
+			</select>	
+		</div>
 
-	<p> <label for "role"> Role:  </label> 
+		<div class="col-md-3"> 
+			<p> <label for "year"> Year: </label> <input type="number" min="1930" max="<?php echo date("Y"); ?>" name="year" value="" required /> </p>
+		</div>
+		
+		<div class="col-md-3"> 
+			<p> <label for "affiliation"> Affiliation: </label> 
 
-	<select name="role">
-		<option value="roles"> Select role </option>
-		<option value="current"> Current Student </option>
-		<option value="alumni"> Alumni </option>
-		<option value="citizen"> Concerned Citizen </option>
-	</select>	
+				<select name="affiliation">
+					<option value="affiliations"> Select Affiliation </option>
+					<option value="norfolk"> Norfolk State</option>
+					<option value="odu"> Old Dominion</option>
+					<option value="tcc"> Tidewater CC </option>
+					<option value="other"> Other </option>
+				</select>	
 
-	<label for "year"> Year: </label> <input type="number" min="1930" max="<?php echo date("Y"); ?>" name="year" value="" required /> </p>
+			</p>
+
+		</div>
+	</div>
+
 	
-	<p> <label for "category"> Category: </label> 
-		<select name="category">
-			<option value="categories"> Categories </option>
-			<option value="general"> General Advice</option>
-			<option value="academic"> Academic</option>
-			<option value="health"> Health </option>
-			<option value="lifestyle"> Lifestyle </option>
-		</select>
+	<div class="row">
+		<div class="col-md-3"> 
+			<p> <label for "category"> Category: </label> 
+				<select name="category">
+					<option value="categories"> Categories </option>
+					<option value="general"> General Advice</option>
+					<option value="academic"> Academic</option>
+					<option value="health"> Health </option>
+					<option value="lifestyle"> Lifestyle </option>
+				</select>
+			</p>
+		</div>
 
-	</p>
+		<div class="col-md-3"> 
+			<p> <label for "tags"> Tags: </label> <input type="text" name="tags" value="" /> </p>
+		</div>
+
+	</div>
 	
-	<p> <label for "tags"> Tags: </label> <input type="text" name="tags" value="" required /> </p>
-	
-	<p> <label for "affiliation"> Affiliation: </label> 
-
-	<select name="affiliation">
-		<option value="affiliations"> Select Affiliation </option>
-		<option value="norfolk"> Norfolk State</option>
-		<option value="odu"> Old Dominion</option>
-		<option value="tcc"> Tidewater CC </option>
-		<option value="other"> Other </option>
-	</select>	
-
-	</p>
 
 	<?php
 
@@ -61,12 +84,15 @@
 	
 	?>
 
-	<input type="Submit" value="Post">
-
-
+	<button type="submit" class="btn btn-success btn-lg">Preview</button>
 
 </form>	
 
+
+</div>
+
+<!-- JAVASCRIPT -->
+<script src="../js/bootstrap.min.js"></script>
 </body>	
 
 </html>
