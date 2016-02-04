863b94ea759cd8c5c488e25b5a40871dd4d237c1
kotut <lynwazim@yahoo.com>
2016-02-04 17:02:35 -0500

    Global Headers
    Introducing Global headers

---
+++ include/header.php
@@ -0,0 +1,3 @@
+<link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="../css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+<link href="../css/custom.css" rel="stylesheet" type="text/css">
--- payforward/index.php
+++ payforward/index.php
@@ -1,39 +1,11 @@
 <html>
 <head> 
 	<title>Pay it Forward </title>
-	<link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
-	<link href="../css/bootstrap-theme.css" rel="stylesheet" type="text/css">
-	<link href="../css/custom.css" rel="stylesheet" type="text/css">
+	<?php include("../include/header.php"); ?>
 </head>
 
 <body data-spy="scroll" data-target="#my-navbar">
-
-  <!-- Navbar -->
-  	<nav class="navbar navbar-inverse navbar-fixed-top" id="my-navbar">
-  		<div class="container">
-  			<div class="navbar-header">
-  				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
-  					<span class="icon-bar"></span>
-  					<span class="icon-bar"></span>
-  					<span class="icon-bar"></span>
-  					<span class="icon-bar"></span>
-  				</button>
-
-  				<a href="" class="navbar-brand">Handshake</a>
-  			</div><!-- Navbar Header-->
-  			<div class="collapse navbar-collapse" id="navbar-collapse">
-  				
-
-  				<ul class="nav navbar-nav">
-  					<li><a href="#meet_up">MeetUp</a> 
-  					<li><a href="#pay_it_forward">Pay it Forward</a> 
-  					<li><a href="#university_socia">University Social</a> 
-  					<li><a href="#ask_questions">Ask Questions</a> 
-  					<li><a href="#ads">ADS</a> 
-  				</ul>
-  			</div>
-  		</div><!-- End Container-->
-  	</nav><!-- End navbar -->
+	<?php include("../include/nav.php"); ?>
 
   	<!-- jumbotron-->
 
