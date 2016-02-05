e3081de12120a526b12752ef87c1abd5a63ad5be
userNsu <vamshika.nsu@gmail.com>
2016-02-05 10:02:21 -0500

    include event.css in php file

--- event.php
+++ event.php
@@ -5,6 +5,7 @@
     <title>Basic Bootstrap Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php include("include/header.php"); ?>
+	<link href="event.css" rel="stylesheet">
 
 </head>
 
