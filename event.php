33c4ba2dd5543c8e52fb062313577ca8e2d7fcc8
userNsu <vamshika.nsu@gmail.com>
2016-02-04 19:23:22 -0500

    Meet Up page UI (main page)

--- event.php
+++ event.php
@@ -12,17 +12,49 @@
 </head>
 
 <body>
+ <!-- Navbar -->
+  	<nav class="navbar navbar-inverse navbar-fixed-top" id="my-navbar">
+  		<div class="container">
+  			<div class="navbar-header">
+  				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+  					<span class="icon-bar"></span>
+  					<span class="icon-bar"></span>
+  					<span class="icon-bar"></span>
+  					<span class="icon-bar"></span>
+  				</button>
 
-		<div class="col-md-9">
+  				<a href="" class="navbar-brand">Handshake</a>
+  			</div><!-- Navbar Header-->
+  			<div class="collapse navbar-collapse" id="navbar-collapse">
+  				
+
+  				<ul class="nav navbar-nav">
+				
+  					<li class="dropdown">  
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">MeetUP <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Create Event</a></li>
+          </ul> 
+		  </li>
+  					<li><a href="#pay_it_forward">Pay it Forward</a> 
+  					<li><a href="#university_socia">University Social</a> 
+  					<li><a href="#ask_questions">Ask Questions</a> 
+  					<li><a href="#ads">ADS</a> 
+  				</ul>
+  			</div>
+  		</div><!-- End Container-->
+  	</nav><!-- End navbar -->
+<div class="csswrap">
+	<div class="col-md-9">
 		<ul>
-  <li>
-    <a href="#">
-      <h2>Title #1</h2>
-      <p>Text Content #1</p>
-    </a>
-  </li>
-  <li>
-    <a href="#">
+			<li>
+				<a href="#">
+				<h2>Title #1</h2>
+				<p>Text Content #1</p>
+				</a>
+			</li>
+			<li>
+			<a href="#">
       <h2>Title #2</h2>
       <p>Text Content #2</p>
     </a>
@@ -53,14 +85,22 @@
   </li>
 </ul >
 </div>
+</div>
 		<div class="col-md-3">
-		<p align=center>Advertisements</p>
+		<ul>
+		<li> <p  align=center style="font-size: 16px;padding-top: 40px;"> Advertisements </p> </li>
+		<li> <a href=#> Computer Architecture Book </a> </li>
+		 <li> <a href=#> Selling shoes(used,size 9)Nike</a> <li>
 		<li> <a href=#> Selling Car, Nissan 2008 </a> </li>
 		<li> <a href=#> Looking for a roommate @ Norfolk </a> </li>
 		<li> <a href=#> Computer Architecture Book </a> </li>
 		 <li> <a href=#> Selling shoes(used,size 9)Nike</a> <li>
+		 </ul>
 		</div>
     
+
 </div>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
