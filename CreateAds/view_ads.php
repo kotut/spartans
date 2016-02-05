4286f1b8eefcd342ff4fc1e3ff798ea370b6b1b8
Julian Lannigan <julian.lannigan@gmail.com>
2016-02-05 13:48:45 -0500

    moar stuff

--- CreateAds/view_ads.php
+++ CreateAds/view_ads.php
@@ -34,30 +34,7 @@ include("../include/db.php");
 
 <body data-spy="scroll" data-target="#my-navbar">
 
-  <!-- Navbar -->
-  	<nav class="navbar navbar-inverse navbar-fixed-top" id="my-navbar">
-  		<div class="container">
-  			<div class="navbar-header">
-  				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
-  					<span class="icon-bar"></span>
-  					<span class="icon-bar"></span>
-  					<span class="icon-bar"></span>
-  					<span class="icon-bar"></span>
-  				</button>
-
-  				<a href="" class="navbar-brand">Handshake</a>
-  			</div><!-- Navbar Header-->
-  			<div class="collapse navbar-collapse" id="navbar-collapse">
-  				<ul class="nav navbar-nav">
-  					<li><a href="#meet_up">Meet &amp; Greet</a>
-  					<li><a href="#pay_it_forward">Pay it Forward</a>
-  					<li><a href="#university_socia">University Social</a>
-  					<li><a href="#ask_questions">Ask Questions</a>
-  					<li><a href="#ads">ADS</a>
-  				</ul>
-  			</div>
-  		</div><!-- End Container-->
-  	</nav><!-- End navbar -->
+   <?php include("../include/nav.php"); ?>
 
 
 
