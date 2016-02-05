e980c01480708d347d19ad02b0c0f8722d32e8f8
Christopher Okonkwo <crisokonkwo@gmail.com>
2016-02-05 11:53:32 -0500

    Comment box

---
+++ commentbox.php
@@ -0,0 +1,66 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        
+        <title>Handshake </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="/css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+<link href="/css/custom.css" rel="stylesheet" type="text/css">
+<link href="/css/style.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" />
+<style>
+    #panel {
+                width: 75%;
+                margin: 0px auto;
+                display: flex;
+                justify-content: flex-end;
+            }
+
+            .menu{
+                width: 620px;
+                margin: 20px auto; padding: 15px;      
+                list-style: none;
+                counter-reset: li;
+                background: #eee;
+                -moz-box-shadow: 0 1px 2px rgba(0,0,0,.1) inset;
+                -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.1) inset;
+                box-shadow: 0 1px 2px rgba(0,0,0,.1) inset;
+                -moz-border-radius: 10px;
+                -webkit-border-radius: 10px;
+                border-radius: 10px;
+            }
+            .menu:after {
+                clear: both;
+            }
+            .menu {
+                zoom:1;
+            }
+            .menu li {
+                position: relative;
+                float: left;
+                cursor: pointer;
+                height: 150px; width: 260px;
+                margin: 25px 0 0 25px;
+                color: #fff;
+            }
+</style>
+</head>
+
+<body>
+ <?php include("include/nav.php"); ?>
+<div class="container">
+        <div id="section">
+            <div id="panel">
+                <ul class="menu">
+                    <script type="text/javascript" async>
+                        function ajaxpath_56b4b3d62c68a(url){
+                            return window.location.href == '' ? url : url.replace('&s=','&s=' + escape(window.location.href));
+                        }(function(){
+                            document.write('<div id="fcs_div_56b4b3d62c68a"><a title="free comment script" href="http://www.freecommentscript.com">&nbsp;&nbsp;<b>Free HTML User Comments</b>...</a></div>');fcs_56b4b3d62c68a=document.createElement('script');fcs_56b4b3d62c68a.type="text/javascript";fcs_56b4b3d62c68a.src=ajaxpath_56b4b3d62c68a((document.location.protocol=="https:"?"https:":"http:")+"//www.freecommentscript.com/GetComments2.php?p=56b4b3d62c68a&s=&Size=10#!56b4b3d62c68a");setTimeout("document.getElementById('fcs_div_56b4b3d62c68a').appendChild(fcs_56b4b3d62c68a)",1);})();</script><noscript><div><a href="http://www.freecommentscript.com" title="free html user comment box">Free Comment Script</a></div></noscript>
+                </ul>
+            </div>
+        </div>
+        </div>
+</body>
+</html>
