88300796225fa02fdbfb2334db11a19fc5adfde2
kotut <lynwazim@yahoo.com>
2016-02-04 21:29:04 -0500

    CSS Header added

--- include/header.php
+++ include/header.php
@@ -1,3 +1,4 @@
 <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
 <link href="../css/bootstrap-theme.css" rel="stylesheet" type="text/css">
 <link href="../css/custom.css" rel="stylesheet" type="text/css">
+<link href="../css/style.css" rel="stylesheet" type="text/css">
