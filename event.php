020d83439a17bbd5d8107bcf3eca66c6680db01b
kotut <lynwazim@yahoo.com>
2016-02-04 22:45:07 -0500

    Event.css added to event.php

--- event.php
+++ event.php
@@ -5,6 +5,7 @@
     <title>Basic Bootstrap Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php include("include/header.php"); ?>
+    <link href="/css/event.css" rel="stylesheet" type="text/css">
 
 </head>
 
