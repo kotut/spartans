3f6116ff1b78d1879ec229e795d258820178e2a5
kotut <lynwazim@yahoo.com>
2016-02-04 21:31:01 -0500

    Repeated header removed

--- meetup.php
+++ meetup.php
@@ -6,12 +6,6 @@
 	<meta charset="utf-8">
 	<title>Handshake </title>
 	<meta name="description" content="Handshake App">
-	<!-- Latest compiled and minified CSS -->
-	 <!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
 </head>
 
 	<style>
