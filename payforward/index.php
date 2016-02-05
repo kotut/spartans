a49b7e9f295d40495844538fdcfcd375416aa654
kotut <lynwazim@yahoo.com>
2016-02-04 19:08:21 -0500

    PiF - Modal Preview

--- payforward/index.php
+++ payforward/index.php
@@ -1,11 +1,14 @@
 <html>
 <head> 
 	<title>Pay it Forward </title>
-	<?php include("../include/header.php"); ?>
+	<link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="../css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+	<link href="../css/custom.css" rel="stylesheet" type="text/css">
 </head>
 
 <body data-spy="scroll" data-target="#my-navbar">
-	<?php include("../include/nav.php"); ?>
+
+ <?php include("include/menu.php"); ?>
 
   	<!-- jumbotron-->
 
@@ -20,13 +23,13 @@
 
 <div class="container">
 
-<form method="POST" action="preview-advice.php">
+<form method="POST" action="process-advice.php">
 
 	<p> <label for "title"> Title Post: </label> <input type="text" class="form-control"  name="title" value="" required /> </p>
 	
 	<p> <label for "advice"> Advice: </label> <br/><textarea name="advice" class="form-control" rows="10" cols="30"></textarea></p>
 	
-	<p> <label for "Name"> Your Name: </label> <input type="text" name="name" value="" required s/></p>
+	<p> <label for "Name"> Your Name: </label> <input type="text" name="name" value="" required/></p>
 		
 	<div class="row">
 		
@@ -91,16 +94,74 @@
 	}
 	
 	?>
-
-	<button type="submit" class="btn btn-success btn-lg">Preview</button>
+	<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Modal</button>
+	<button type="submit" class="btn btn-success btn-lg" id="preview_view">Preview</button>
 
 </form>	
 
 
 </div>
 
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">PREVIEW ENTRY</h4>
+      </div>
+      <div class="modal-body">
+			<p><strong>Advice Title: </strong><span id="preview_title"></span></p> 
+			<p><strong>Your Advice: </strong><span id="preview_advice"></span></p>
+			<p><strong>Your Information: </strong> 
+				<span id="preview_name"></span>,  
+				<span id="preview_role"></span>, 
+				<span id="preview_affiliation"></span>,  
+				<span id="preview_year"></span> 
+			</p>
+
+			<p><strong>Tags: </strong>
+				<span id="preview_tags"></span> <strong>Category:</strong> <span id="preview_category"></span> 
+			</p>
+
+      </div>
+      <div class="modal-footer">
+      	<button type="button" class="btn btn-default" data-dismiss="modal">Edit</button>
+      	<button type="button" class="btn btn-default" id="publish">Publish</button>
+
+      </div>
+    </div>
+
+  </div>
+</div>
+
 <!-- JAVASCRIPT -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="../js/bootstrap.min.js"></script>
+<script>
+	$(document).ready(function(){
+		$('#preview_view').click(function(){
+			$('#preview_title').text($('input[name="title"]').val());
+			$('#preview_advice').text($('textarea[name="advice"]').val());						
+			$('#preview_name').text($('input[name="name"]').val());
+			$('#preview_year').text($('input[name="year"]').val());
+			$('#preview_affiliation').text($('select[name="affiliation"]').val());
+			$('#preview_role').text($('select[name="role"]').val());
+			$('#preview_category').text($('select[name="category"]').val());
+			$('#preview_tags').text($('input[name="tags"]').val());
+			$('#myModal').modal({show:true});
+			return false;
+		});
+
+		$('#publish').click(function(){
+			$('form').submit();
+			return false;
+		});
+
+	})
+</script>
 </body>	
 
 </html>
