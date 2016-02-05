10e3b9aabb4d11ab678214a7eb6f0adb373c2c04
kotut <lynwazim@yahoo.com>
2016-02-04 22:23:33 -0500

    Corrections

--- event.php
+++ event.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>Basic Bootstrap Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php include("../include/header.php"); ?>
+    <?php include("/include/header.php"); ?>
 
 </head>
 
